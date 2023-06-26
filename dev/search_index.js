407947c5d53d8ec4640bbd21c020b4e0aa4524d3
Documenter.jl <documenter@juliadocs.github.io>
2023-06-26 18:53:16 +0000

    build based on 12b222f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MUSCLE","category":"page"},{"location":"#MUSCLE","page":"Home","title":"MUSCLE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MUSCLE.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MUSCLE]","category":"page"}]
+}
